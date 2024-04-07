70fd5dc5f05bc18b12baa474a80e44c01b329e2e
irpolg <150624082+irpolg@users.noreply.github.com>
2024-04-07 18:02:33 +0000

    Deploying to gh-pages from @ irpolg/goit-js-hw-11@32438b980530fb8a3e0fd957f0cd4870301aacf2 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,26 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import{i as h,S as c}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const l=document.querySelector(".js-search-form"),p=document.querySelector(".js-list");document.querySelector(".search-input");l.addEventListener("submit",d);function d(s){s.preventDefault();const{seachImage:r}=s.currentTarget.elements;console.log("seachImage = те, що ввели в поле пошуку ",r.value),m(r.value).then(o=>{console.log("data - результат пошуку",o),console.log("data - кількість результат пошуку",o.hits.length),o.hits.length===0&&h.error({title:"Error",titleColor:"white",backgroundColor:"#EF4040",messageColor:"#FFFFFF",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),p.innerHTML=f(o.hits),new c(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250,captionPosition:"bottom"})}).catch(o=>{alert(o)}),l.reset()}function m(s=""){const r="43270282-4a5d06b91258db09a976f913c",o=new URLSearchParams({key:r,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`https://pixabay.com/api?${o}`).then(e=>{if(!e.ok)throw new Error("Ooooops!");return e.json()})}function f(s){return s.map(({id:r,webformatURL:o,tags:i,largeImageURL:e,likes:t,views:a,comments:n,downloads:u})=>`
+        <li class="gallery-item" data-id=${r} heigth="200">
+            <a class="gallery-link" href=${e}>
+                <img src="${o}" alt="${i}" width="360">
+            </a>
+            <div class="gallery-word">
+                <div class="gallery-opis">
+                    <h2 class="title">Likes</h2>
+                    <p  class="count">${t}</p>
+                </div>
+                <div class="gallery-opis">
+                    <h2 class="title">Views</h2>
+                    <p  class="count">${a}</p>
+                </div>
+                <div class="gallery-opis">
+                    <h2 class="title">Comments</h2>
+                    <p  class="count">${n}</p>
+                </div>
+                <div class="gallery-opis">
+                    <h2 class="title">Downloads</h2>
+                    <p  class="count">${u}</p>
+                </div>
+            </div>
+        </li>
+    `).join("")}new c("js-list a",{captionPosition:"center",captionsData:"alt",captionsDelay:250});
 //# sourceMappingURL=commonHelpers.js.map
